2696bc97824bafc146d983d30a2efecf7eb830df
david hauri <chubbson@gmail.com>
2015-06-20 23:25:41 +0200

    updated usage of fldviewer

--- 0_hpctf_sem/fldviewer.c
+++ 0_hpctf_sem/fldviewer.c
@@ -12,26 +12,37 @@
 
 #define MAXPLAYER 1878 //1878 // -> somecolor, added new fieldprint. 1878 different stante could be printed so that the new max player cnt
 
-int verbose = FALSE; 
+
+int verbose = FALSE;
 int updms = 5000;
+int rawfld = FALSE;
+int colfld = TRUE;
 
 void usage(int argc, char const *argv[])
 {
-  printf("%s\n", "fldviewer");
+  printf("USAGE:\n\n%s\n", argv[0]);
   printf("\t-v\tVerbose\n");
   printf("\t-ms=5000\tUpdate in ms, default 5000\n");
-
-  for (int i = 0; i < argc; ++i)
+  printf("\t-raw\tPrint field Raw with x y coords player, if not set print graphical\n");
+  printf("\t-col\tPrint field Colored, if this is set explicity, raw and col are allowed(-raw -col)\n");
+ 
+  for (int i = 1; i < argc; ++i)
   {
     if(strcmp(argv[i], "-v") == 0)
     {  
       verbose = TRUE; 
-      continue;
     }
-    if(strncmp(argv[i], "-ms=", 4) == 0)
+    else if(strcmp(argv[i], "-raw") == 0)
+    {  
+      rawfld = TRUE; 
+    }
+    else if(strcmp(argv[i], "-col") == 0)
+    {  
+      colfld = TRUE; 
+    }
+    else if(strncmp(argv[i], "-ms=", 4) == 0)
     {
       sscanf(argv[i], "-ms=%d", &updms);
-      continue;
     }
   }
 } 
@@ -52,20 +63,33 @@ static void updsubscriber_task(void *args, zctx_t *ctx, void *pipe)
       sequence++;
       kvmsg_t *kvmsg = kvmsg_recv (updsub);
       if (!kvmsg)
+      {
+        errno = zmq_errno(); 
+        if (errno == EAGAIN) 
+        { printf("I: EAGAIN! continue\n");
+          continue; } 
+        if (errno == ETERM) 
+        { printf ("I: Terminated!\n"); 
+          break; 
+        } 
+        printf ("E: (%d) %s\n", errno, strerror(errno)); 
         break;          //  Interrupted
-
+      }
+ 
       if (verbose)
+      {
         kvmsg_dump(kvmsg);
+      }
       
       kvmsg_store (&kvmsg, kvmap);
   }
 
   zsocket_destroy (ctx, updsub);
+  zsys_interrupted = 1; 
 
   char buf[100];
   snprintf(buf, 100, "updsubscriber_task, Interrupted\n%" PRId64 "messages in\n", sequence);
   puts(buf);
-  //zstr_send(pipe, buf);
 }
 
 
